c240a42705fe562257ae76ad351fc88ea75bfb8c
Zoltan Komives <zoltan.komives@gmail.com>
2013-07-15 14:52:21 +0100

    added test with embedded xml (as JS string variable)

--- tests/runtest.js
+++ tests/runtest.js
@@ -4,12 +4,29 @@ MEI2VF.RunTest = function(test_case, canvas, titleElem){
   $(titleElem).html(test_case.title);
 
   //load the xml file...
-  Vex.LogInfo("Running Test Case Title: '" + test_case.title + "' MEI-XML: '" + test_case.mei_xml + "'");
-  xmlDoc=loadXMLDoc(test_case.mei_xml);
+  Vex.LogInfo("Running Test Case Title: '" + test_case.title + "' MEI-XML:" + (test_case.mei_xml_file?"'"+test_case.mei_xml_file+"'":'embedded'));
+  
+  if (test_case.mei_xml_file) {
+    xmlDoc=loadXMLDoc(test_case.mei_xml_file);
+  } else {
+    
+    if (window.DOMParser)
+      {
+      parser=new DOMParser();
+      xmlDoc=parser.parseFromString(test_case.mei_xml_string,"text/xml");
+      }
+    else // Internet Explorer
+      {
+      xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+      xmlDoc.async=false;
+      xmlDoc.loadXML(text); 
+      }
+  }
+  
   if (xmlDoc) { 
     Vex.LogInfo('MEI-XML loaded.'); 
   } else {
-    //TODO: throw exception
+    Vex.LogInfo('TEST FAILED: MEI-XML cannot be loaded.'); 
   }
 
   //... and render it onto the canvas
@@ -19,4 +36,3 @@ MEI2VF.RunTest = function(test_case, canvas, titleElem){
   Vex.LogInfo('Done (' + test_case.title + ')');
 	
 }
-
