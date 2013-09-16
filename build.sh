f882f142d70de91c0f935683fa0744b4b7c8aa86
Zoltan Komives <zolaemil@gmail.com>
2013-09-16 11:43:40 +0200

    include licence note in build

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-temp_js=mei2vf-combined.js
-cat meilib.js           > $temp_js
 if [ ! -d build ]; then
   mkdir build;
 fi
+
+temp_js=build/mei2vf-combined.js
+temp_min=build/temp-min.js
+out_js=build/meitovexflow-min.js
+cat meilib.js           >> $temp_js
 cat tables.js           >> $temp_js
 cat meitovexflow.js     >> $temp_js
 cat EventLink.js        >> $temp_js
@@ -13,7 +16,10 @@ cat StaffInfo.js        >> $temp_js
 cat StaveConnector.js   >> $temp_js
 cat StaveVoices.js      >> $temp_js
 
-java -jar support/yuicompressor-2.4.7.jar $temp_js -o build/meitovexflow-min.js --disable-optimizations
+java -jar support/yuicompressor-2.4.7.jar $temp_js -o $temp_min --disable-optimizations
+cat apache2.0.js > $out_js
+cat $temp_min >> $out_js
 
 rm $temp_js
+rm $temp_min
 
