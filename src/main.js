d59fd0550e15600d2a60ab6d2d6aa48a6f93b6d1
aerhard <a_erhard@gmx.de>
2014-03-11 20:41:25 +0100

    added file

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+MEI2VF = (function(m2v, VF, $, undefined) {
+
+  m2v.getRenderedMeasures = function() {
+    return m2v.rendered_measures;
+  };
+  
+  return {
+    render_notation: m2v.render_notation,
+    getRenderedMeasures: m2v.getRenderedMeasures
+  };
+  
+}(MEI2VF || {}, Vex.Flow, jQuery));
