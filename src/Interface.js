c4a074dcf172dfdce596e25b3a0738ed762102d0
aerhard <a_erhard@gmx.de>
2014-07-05 07:57:19 +0200

    restructured Interface.js

--- src/Interface.js
+++ src/Interface.js
@@ -21,47 +21,45 @@ var MEI2VF = ( function(m2v, MeiLib, VF, $, undefined) {
         getStaffArea : function() {
           return m2v.Converter.prototype.getStaffArea();
         }
+      },
+      /**
+       * @property
+       */
+      rendered_measures: null,
+      /**
+       * Main rendering function. Uses the Converter's prototype as a
+       * singleton. No scaling; page layout information in the MEI code is ignored.
+       * @param {XMLDocument} xmlDoc The MEI XML Document
+       * @param {XMLElement} target An svg or canvas element
+       * @param {Number} width The width of the print space in pixels
+       * @param {Number} height The height of the print space in pixels
+       * @param {Number} backend Set to Vex.Flow.Renderer.Backends.RAPHAEL to
+       * render to a Raphael context; if falsy, Vex.Flow.Renderer.Backends.CANVAS
+       * is set
+       * @param {Object} options The options passed to the converter. For a list, see
+       * {@link MEI2VF.Converter MEI2VF.Converter}
+       */
+      render_notation: function (xmlDoc, target, width, height, backend, options) {
+        var ctx;
+        var cfg = options || {};
+
+        ctx = new VF.Renderer(target, backend || VF.Renderer.Backends.CANVAS).getContext();
+
+        width = width || 800;
+        height = height || 350;
+
+        if (+backend === VF.Renderer.Backends.RAPHAEL) {
+          ctx.paper.setSize(width, height);
+        }
+
+        cfg.page_width = width;
+
+        this.Converter.initConfig(cfg);
+        this.Converter.process(xmlDoc[0] || xmlDoc);
+        this.Converter.draw(ctx);
+        this.rendered_measures = this.Converter.getAllVexMeasureStaffs();
+
       }
     };
+  
   }(MEI2VF || {}, MeiLib, Vex.Flow, jQuery));
-
-/**
- * @property
- */
-MEI2VF.rendered_measures = null;
-
-/**
- * Basic rendering function. Uses the m2v.Converter's prototype as a
- * singleton. No scaling; page layout information in the MEI code is ignored.
- * @param {XMLDocument} xmlDoc The MEI XML Document
- * @param {XMLElement} target An svg or canvas element
- * @param {Number} width The width of the print space in pixels
- * @param {Number} height The height of the print space in pixels
- * @param {Number} backend Set to Vex.Flow.Renderer.Backends.RAPHAEL to
- * render to a Raphael context; if falsy, Vex.Flow.Renderer.Backends.CANVAS
- * is set
- * @param {Object} options The options passed to the converter. For a list, see
- * {@link MEI2VF.Converter MEI2VF.Converter}
- */
-MEI2VF.render_notation = function(xmlDoc, target, width, height, backend, options) {
-  var ctx;
-  var cfg = options || {};
-
-  ctx = new Vex.Flow.Renderer(target, backend || Vex.Flow.Renderer.Backends.CANVAS).getContext();
-
-  width = width || 800;
-  height = height || 350;
-
-  if (+backend === Vex.Flow.Renderer.Backends.RAPHAEL) {
-    ctx.paper.setSize(width, height);
-  }
-
-  cfg.page_width = width;
-
-  this.Converter.initConfig(cfg);
-  this.Converter.process(xmlDoc[0] || xmlDoc);
-  this.Converter.draw(ctx);
-  this.rendered_measures = this.Converter.getAllVexMeasureStaffs();
-
-};
-
